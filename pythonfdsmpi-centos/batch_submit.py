d872775e3e8e38c06dac4fa5557370e4c2178859
Emmanuel  Auffray <emmanuel.auffray@theta.co.nz>
2018-06-19 10:09:50 +1200

    fix type in sum of instances

--- pythonfdsmpi-centos/batch_submit.py
+++ pythonfdsmpi-centos/batch_submit.py
@@ -288,7 +288,7 @@ def create_pool(batch_service_client, pool_id,
         target_low_priority_nodes=_POOL_NODE_COUNT_LOW,
         target_dedicated_nodes=_POOL_NODE_COUNT,
         enable_inter_node_communication=1,
-        max_tasks_per_node=1, # as per mpi article
+        max_tasks_per_node=1,  # as per mpi article
         start_task=batch.models.StartTask(
             command_line=common.helpers.wrap_commands_in_shell('linux',
                                                                task_commands),
@@ -371,6 +371,8 @@ def add_tasks(batch_service_client, job_id, input_files,
                        output_container_name,
                        output_container_sas_token)]
 
+        total_instances = int(_POOL_NODE_COUNT) + int(_POOL_NODE_COUNT_LOW)
+        print(f"{total_instances} and type {type(total_instances)} ")
         tasks.append(batch.models.TaskAddParameter(
             'FDS',
             common.helpers.wrap_commands_in_shell('linux', command),
@@ -378,7 +380,7 @@ def add_tasks(batch_service_client, job_id, input_files,
             multi_instance_settings=(batch.models.MultiInstanceSettings(
                 coordination_command_line=common.helpers.wrap_commands_in_shell(
                     'linux', task_commands),
-                number_of_instances=_POOL_NODE_COUNT+_POOL_NODE_COUNT_LOW)
+                number_of_instances=total_instances)
             ),
             user_identity=batch.models.UserIdentity(user_name='fdsuser'),
         )
@@ -475,7 +477,7 @@ def main(argv):
     conffile = ''
     mpicount = 1
     openMPcount = 1
-    nodes = 1
+    nodes = 0
     lowprio = 0
     rdma = 0
     vmsku = 'STANDARD_A8'
@@ -516,6 +518,12 @@ def main(argv):
             vmsku = arg
 
     if inputfile == '':
+        print(helpline)
+        sys.exit()
+
+    totalnodes = int(nodes) + int(lowprio)
+    if (totalnodes) == 0:
+        print("Needs at least one node!")
         print(helpline)
         sys.exit()
 
